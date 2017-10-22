225b270a163660dcd7180a0ce8177a836be6e8f2
Diego Paiva <diego.paiva13@gmail.com>
2017-10-22 21:01:31 -0200

    feito cadastro de reserva

--- app/src/main/java/com/example/diego/feiradelivros/Participante.java
+++ app/src/main/java/com/example/diego/feiradelivros/Participante.java
@@ -5,23 +5,32 @@ import android.widget.Toast;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Participante implements Serializable {
     private String nome;
     private String email;
-    private String horaEntrada=null;
-    private String horaSaida=null;
-    private SimpleDateFormat dateFormat_hora = new SimpleDateFormat("HH:mm:ss");
+    private String horaEntrada;
+    private String horaSaida;
+    private SimpleDateFormat dateFormatHora;
+    private ArrayList<Livro> livrosReservados;
 
     public Participante() {
-        // construtor vazio
+        this.livrosReservados = new ArrayList<Livro>();
+        this.horaEntrada = null;
+        this.horaSaida = null;
+        this.dateFormatHora = new SimpleDateFormat("HH:mm:ss");
     }
 
     public Participante(String nome, String email) {
         this.nome = nome;
         this.email = email;
+        this.horaEntrada = null;
+        this.livrosReservados = new ArrayList<Livro>();
+        this.horaSaida = null;
+        this.dateFormatHora = new SimpleDateFormat("HH:mm:ss");
     }
 
     public String getNome() {
@@ -48,15 +57,29 @@ public class Participante implements Serializable {
         return horaSaida;
     }
 
+    public ArrayList<Livro> getLivrosReservados() {
+        return livrosReservados;
+    }
+
+    public void setLivrosReservados(ArrayList<Livro> livrosReservados) {
+        this.livrosReservados = livrosReservados;
+    }
+
+    public void adicionarLivroReservado(Livro livro) {
+        if(livro != null) {
+            this.livrosReservados.add(livro);
+        }
+    }
+
     public void registraHora(Context context){
         if(horaEntrada==null){
             Date hora = Calendar.getInstance().getTime();
-            horaEntrada = dateFormat_hora.format(hora);
+            horaEntrada = dateFormatHora.format(hora);
             Toast.makeText(context, "Hora de entrada registrada!", Toast.LENGTH_LONG).show();
         }
         else if(horaSaida==null){
             Date hora = Calendar.getInstance().getTime();
-            horaSaida = dateFormat_hora.format(hora);
+            horaSaida = dateFormatHora.format(hora);
             Toast.makeText(context, "Hora de saída registrada!", Toast.LENGTH_LONG).show();
         }
         else{
@@ -65,6 +88,19 @@ public class Participante implements Serializable {
             Toast.makeText(context, "Horários limpos!", Toast.LENGTH_LONG).show();
         }
     }
+
+    public boolean estaPresente() {
+        // Se o participante ja entrou mas ainda nao saiu, ele está presente
+        if(this.horaEntrada != null && this.horaSaida == null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
 
     // Método para que, ao realizar a listagem, seja exibido apenas o nome do participante
     @Override
